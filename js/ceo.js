1cb1380d3e94d23ab307545dfd2c9c6cf2b60d89
robinpaulus <112939299+robinpaulus@users.noreply.github.com>
2024-07-23 20:10:59 +0700

    Add files via upload

---
+++ js/ceo.js
@@ -0,0 +1,9 @@
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Place your logic for handling the form submission here
+    // For example, you can use AJAX to send the form data to a server.
+    // You can also display a thank you message to the user.
+  });
